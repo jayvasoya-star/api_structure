535b0f32a4540d2728c16805e20dc702c62fb2e0
jaydeep vasoya <156075552+jayvasoya-star@users.noreply.github.com>
2025-03-22 00:51:04 +0530

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,3 +1,18 @@
+import 'package:flutter/material.dart';
+
 void main() {
- setupLocator();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'API Data Fetch Example',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: ApiDataFetchScreen(endpoint: 'posts', screenName: 'Posts Screen'),
+    );
+  }
 }
